a8d7b9efddbc781dc12732155552d6bb729da461
denisa <denisa.hutter03@e-uvt.ro>
2023-11-23 10:30:38 +0200

    test

---
+++ src/main/java/com/example/test1sp/Battalion.java
@@ -0,0 +1,34 @@
+package com.example.test1sp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Battalion implements Creature{
+
+    private List<Creature> creatures;
+
+    public Battalion() {
+        this.creatures = new ArrayList<>();
+    }
+
+    public void addCreature(Creature creature) {
+        creatures.add(creature);
+    }
+
+    public void removeCreature(Creature creature) {
+        creatures.remove(creature);
+    }
+
+    @Override
+    public void attack() {
+        for (Creature creature : creatures) {
+            creature.attack();
+        }
+    }
+
+    @Override
+    public void defend() {
+        for (Creature creature : creatures) {
+            creature.defend();
+        }
+    }
+}
